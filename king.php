4c6a6cf1cdedc036aa5d7a2f5076ec1dfe31fcef
valoeta <52375174+valoeta@users.noreply.github.com>
2019-08-01 17:24:56 +0700

    Create king.php

---
+++ king.php
@@ -0,0 +1,12 @@
+<?php
+// Warna Terminal
+$biru = "\e[34m";
+$kuning = "\e[33m";                                             $cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$m = "\e[91m";
+$b = " \033[1m";
+echo " $cyan$b PROXY GENERATOR (FEEE VERSION ^_^):D \n >";
+echo " $m INFO! ";
+echo file_get_contents("https://pastebin.com/raw/2yMvcPzL");
+?>
