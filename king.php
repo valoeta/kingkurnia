798e783e337a690a0b70f7169c4ca511f03c3319
valoeta <52375174+valoeta@users.noreply.github.com>
2019-08-03 23:11:33 +0700

    Update king.php

--- king.php
+++ king.php
@@ -7,6 +7,6 @@ $hijau = "\e[92m";
 $m = "\e[91m";
 $b = " \033[1m";
 echo " $cyan$b 1 MINGGU GRATIS PROXY INDONESIA \n >";
-echo " $m INFO! ";
+echo " $hijau LIST PROXY ";
 echo file_get_contents("https://pastebin.com/raw/g5ByCeRF");
 ?>
